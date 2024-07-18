35338f81e7e30a2e163a2fb6f7df73e35c43e645
Andrea D <andreacollinsj@gmail.com>
2024-07-18 14:37:41 -0400

    added tag to ami

--- chapter_2/main.tf
+++ chapter_2/main.tf
@@ -7,4 +7,8 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-04a81a99f5ec58529"
     instance_type = "t2.micro"
+
+    tags = {
+        Name = "terraform-example-oreilly"
+    }
 }
